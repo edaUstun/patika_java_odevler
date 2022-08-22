a9c46592de6cfef3c03aea654e179654bb7fa04b
edaUstun <edaustun1141@gmail.com>
2022-08-22 14:56:15 +0300

    odev3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import Hipotenus.Hipotenus;
 import kdv.Kdv;
+import taksi.Taksimetre;
 
 public class Main {
     public static void main(String[] args) {
 
         //Kdv kdv = new Kdv();
 
-        Hipotenus hipotenus = new Hipotenus();
+        //Hipotenus hipotenus = new Hipotenus();
+
+        Taksimetre taksimetre = new Taksimetre();
     }
 }
---
+++ src/taksi/Taksimetre.java
@@ -0,0 +1,24 @@
+package taksi;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+
+    public Taksimetre() {
+        double mesafe;
+        double perKm = 2.20;
+        double startPrice = 10;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mesafeyi KM cinsinden giriniz: ");
+        mesafe = scanner.nextDouble();
+
+        double total = startPrice + (mesafe*perKm);
+
+        if(total<20) {
+            System.out.println("Odenecek tutar 20 TL");
+        } else if (total>20) {
+            System.out.println("Ucret:" + total);
+        }
+    }
+}
